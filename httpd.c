308c3629c2bd157f43290d8613c8f5423e816a90
liuyunbin <liuyunbin6537@gmail.com>
2017-09-01 11:32:59 +0800

    添加注释，修改部分结构

--- httpd.c
+++ httpd.c
@@ -4,14 +4,6 @@
  * CSE 4344 (Network concepts), Prof. Zeigler
  * University of Texas at Arlington
  */
-/* This program compiles for Sparc Solaris 2.6.
- * To compile for Linux:
- *  1) Comment out the #include <pthread.h> line.
- *  2) Comment out the line that defines the variable newthread.
- *  3) Comment out the two lines that run pthread_create().
- *  4) Uncomment the line that runs accept_request().
- *  5) Remove -lsocket from the Makefile.
- */
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -26,50 +18,46 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 
-#define ISspace(x) isspace((int)(x))
-
 #define SERVER_STRING "Server: jdbhttpd/0.1.0\r\n"
 
-/* void accept_request(int); */
 void* accept_request(void*);
 void bad_request(int);
 void cat(int, FILE *);
 void cannot_execute(int);
 void error_die(const char *);
 void execute_cgi(int, const char *, const char *, const char *);
-int get_line(int, char *, int);
+int  get_line_from_sock(int, char *, int);
 void headers(int, const char *);
 void not_found(int);
 void serve_file(int, const char *);
-int startup(u_short *);
+int  get_server_socket(u_short *);
 void unimplemented(int);
 
-/**********************************************************************/
-/* A request has caused a call to accept() on the server port to
- * return.  Process the request appropriately.
- * Parameters: the socket connected to the client */
-/**********************************************************************/
-/* void accept_request(int client) */
+/***************************************/
+/*                                     */
+/*      线程函数，处理与客户端的交互   */
+/*                                     */
+/***************************************/
 void* accept_request(void* client_sock) {
-    int client = *(int*)client_sock;
-    char buf[1024];
-    int numchars;
-    char method[255];
-    char url[255];
-    char path[512];
-    size_t i, j;
-    struct stat st;
-    int cgi = 0;      /* becomes true if server decides this is a CGI
-                    * program */
+    int     client = *(int*)client_sock;
+    char    buf[1024];     /**********************/
+    char    *method;       /* 请求方法           */
+    char    *url;          /* 请求资源           */
+    char    path[512];     /* 请求资源实际路径   */
+    size_t  i;             /**********************/
+    struct  stat st;
+    int     cgi = 0;      /* becomes true if server decides this is a CGI program */
     char *query_string = NULL;
 
-    numchars = get_line(client, buf, sizeof(buf));
-    i = 0; j = 0;
-    while (!ISspace(buf[j]) && (i < sizeof(method) - 1)) {
-        method[i++] = buf[j++];
-    }
-    method[i] = '\0';
+    free(client_sock);
 
+    get_line_from_sock(client, buf, sizeof(buf));
+
+    method = buf;
+    for (i = 0; !isspace(buf[i]) && buf[i] != '\0'; ++i)
+        ;
+    buf[i++] = '\0';
+    /* 请求资源未实现  */
     if (strcasecmp(method, "GET") && strcasecmp(method, "POST")) {
         unimplemented(client);
         return NULL;
@@ -78,39 +66,36 @@ void* accept_request(void* client_sock) {
     if (strcasecmp(method, "POST") == 0)
         cgi = 1;
 
-    i = 0;
-    while (ISspace(buf[j]) && (j < sizeof(buf)))
-        j++;
-    while (!ISspace(buf[j]) && (i < sizeof(url) - 1) && (j < sizeof(buf))) {
-        url[i++] = buf[j++];
-    }
-    url[i] = '\0';
+    while (isspace(buf[i]) && buf[i] != '\0')
+        ++i;
+    url = buf + i;
+    while (!isspace(buf[i]) && buf[i] != '\0')
+        ++i;
+    buf[i] = '\0';
 
     if (strcasecmp(method, "GET") == 0) {
         query_string = url;
-        while ((*query_string != '?') && (*query_string != '\0'))
-            query_string++;
+        while (*query_string != '?' && *query_string != '\0')
+            ++query_string;
         if (*query_string == '?') {
             cgi = 1;
             *query_string = '\0';
-            query_string++;
+            ++query_string;
         }
     }
 
     sprintf(path, "htdocs%s", url);
     if (path[strlen(path) - 1] == '/')
         strcat(path, "index.html");
+    /* 请求资源未找到  */
     if (stat(path, &st) == -1) {
-        while ((numchars > 0) && strcmp("\n", buf))  /* read & discard headers */
-            numchars = get_line(client, buf, sizeof(buf));
+        while (get_line_from_sock(client, buf, sizeof(buf)) > 0 && strcmp("\n", buf))  /* read & discard headers */
+            ;
         not_found(client);
     }
     else {
-        if ((st.st_mode & S_IFMT) == S_IFDIR)
-            strcat(path, "/index.html");
-        if ((st.st_mode & S_IXUSR) ||
-            (st.st_mode & S_IXGRP) ||
-            (st.st_mode & S_IXOTH)    )
+        /* 查看文件是否有可执行权限  */
+        if ((st.st_mode&S_IXUSR) || (st.st_mode&S_IXGRP) || (st.st_mode&S_IXOTH))
             cgi = 1;
         if (!cgi)
             serve_file(client, path);
@@ -152,7 +137,7 @@ void cat(int client, FILE *resource) {
      char buf[1024];
 
      fgets(buf, sizeof(buf), resource);
-    while (!feof(resource)) {
+     while (!feof(resource)) {
         send(client, buf, strlen(buf), 0);
         fgets(buf, sizeof(buf), resource);
     }
@@ -175,22 +160,16 @@ void cannot_execute(int client) {
     send(client, buf, strlen(buf), 0);
 }
 
-/**********************************************************************/
-/* Print out an error message with perror() (for system errors; based
- * on value of errno, which indicates system call errors) and exit the
- * program indicating an error. */
-/**********************************************************************/
 void error_die(const char *sc) {
     perror(sc);
     exit(1);
 }
 
-/**********************************************************************/
-/* Execute a CGI script.  Will need to set environment variables as
- * appropriate.
- * Parameters: client socket descriptor
- *             path to the CGI script */
-/**********************************************************************/
+/******************************************/
+/*                                        */
+/*               执行 cgi 脚本            */
+/*                                        */
+/******************************************/
 void execute_cgi(int client, const char *path, const char *method, const char *query_string) {
     char buf[1024];
     int cgi_output[2];
@@ -206,15 +185,15 @@ void execute_cgi(int client, const char *path, const char *method, const char *q
     buf[1] = '\0';
     if (strcasecmp(method, "GET") == 0) {
         while ((numchars > 0) && strcmp("\n", buf))  /* read & discard headers */
-            numchars = get_line(client, buf, sizeof(buf));
+            numchars = get_line_from_sock(client, buf, sizeof(buf));
     }
     else    /* POST */ {
-        numchars = get_line(client, buf, sizeof(buf));
+        numchars = get_line_from_sock(client, buf, sizeof(buf));
         while ((numchars > 0) && strcmp("\n", buf)) {
             buf[15] = '\0';
             if (strcasecmp(buf, "Content-Length:") == 0)
                 content_length = atoi(&(buf[16]));
-            numchars = get_line(client, buf, sizeof(buf));
+            numchars = get_line_from_sock(client, buf, sizeof(buf));
         }
         if (content_length == -1) {
             bad_request(client);
@@ -225,16 +204,7 @@ void execute_cgi(int client, const char *path, const char *method, const char *q
     sprintf(buf, "HTTP/1.0 200 OK\r\n");
     send(client, buf, strlen(buf), 0);
 
-    if (pipe(cgi_output) < 0) {
-        cannot_execute(client);
-        return;
-    }
-    if (pipe(cgi_input) < 0) {
-        cannot_execute(client);
-        return;
-    }
-
-    if ( (pid = fork()) < 0 ) {
+    if (pipe(cgi_output) < 0 || pipe(cgi_input) < 0 || (pid=fork()) < 0) {
         cannot_execute(client);
         return;
     }
@@ -277,45 +247,25 @@ void execute_cgi(int client, const char *path, const char *method, const char *q
     }
 }
 
-/**********************************************************************/
-/* Get a line from a socket, whether the line ends in a newline,
- * carriage return, or a CRLF combination.  Terminates the string read
- * with a null character.  If no newline indicator is found before the
- * end of the buffer, the string is terminated with a null.  If any of
- * the above three line terminators is read, the last character of the
- * string will be a linefeed and the string will be terminated with a
- * null character.
- * Parameters: the socket descriptor
- *             the buffer to save the data in
- *             the size of the buffer
- * Returns: the number of bytes stored (excluding null) */
-/**********************************************************************/
-int get_line(int sock, char *buf, int size) {
+/******************************************************/
+/*                                                    */
+/* 从指定套接字读取一行字符，将 \r\n 或 \r 换成 \n    */
+/*                                                    */
+/******************************************************/
+int get_line_from_sock(int sock, char *buf, int size) {
     int i = 0;
-    char c = '\0';
-    int n;
+    char c;
 
-    while ((i < size - 1) && (c != '\n')) {
-        n = recv(sock, &c, 1, 0);
-  /* DEBUG printf("%02X\n", c); */
-        if (n > 0) {
-            if (c == '\r') {
-                n = recv(sock, &c, 1, MSG_PEEK);
-    /* DEBUG printf("%02X\n", c); */
-                if ((n > 0) && (c == '\n'))
-                    recv(sock, &c, 1, 0);
-                else
-                    c = '\n';
-            }
-            buf[i] = c;
-            i++;
-        }
-        else
-            c = '\n';
-    }
+    while (i < size - 1 && recv(sock, &c, 1, 0) > 0 && c != '\n' && c != '\r')
+        buf[i++] = c;
+    /* MSG_PEEK 表示数据并未从缓存中删除,下一次读取的还是该字符 */
+    if (c == '\r' && recv(sock, &c, 1, MSG_PEEK) > 0 && c == '\n')
+        recv(sock, &c, 1, 0);
+    if (c == '\n')
+        buf[i++] = c;
     buf[i] = '\0';
  
-    return(i);
+    return i;
 }
 
 /**********************************************************************/
@@ -337,9 +287,11 @@ void headers(int client, const char *filename) {
     send(client, buf, strlen(buf), 0);
 }
 
-/**********************************************************************/
-/* Give a client a 404 not found status message. */
-/**********************************************************************/
+/*******************************/
+/*                             */
+/*  请求资源未找到 404 错误    */
+/*                             */
+/*******************************/
 void not_found(int client) {
     char buf[1024];
 
@@ -363,70 +315,64 @@ void not_found(int client) {
     send(client, buf, strlen(buf), 0);
 }
 
-/**********************************************************************/
-/* Send a regular file to the client.  Use headers, and report
- * errors to client if they occur.
- * Parameters: a pointer to a file structure produced from the socket
- *              file descriptor
- *             the name of the file to serve */
-/**********************************************************************/
+/*************************************/
+/*                                   */ 
+/*    向客户端传送文件               */
+/*                                   */
+/*************************************/
 void serve_file(int client, const char *filename) {
-    FILE *resource = NULL;
-    int numchars = 1;
-    char buf[1024];
+    FILE *resource;
+    char buf[1024] = "A";
 
-    buf[0] = 'A'; buf[1] = '\0';
-    while ((numchars > 0) && strcmp("\n", buf))  /* read & discard headers */
-        numchars = get_line(client, buf, sizeof(buf));
+    /* 读取并忽略请求头部  */
+    while (get_line_from_sock(client, buf, sizeof(buf)) > 0 && strcmp("\n", buf))
+        ;
 
     resource = fopen(filename, "r");
-    if (resource == NULL)
+    if (resource == NULL) {
         not_found(client);
-    else {
-        headers(client, filename);
-        cat(client, resource);
+        return ;
     }
+    headers(client, filename);
+    cat(client, resource);
     fclose(resource);
 }
 
-/**********************************************************************/
-/* This function starts the process of listening for web connections
- * on a specified port.  If the port is 0, then dynamically allocate a
- * port and modify the original port variable to reflect the actual
- * port.
- * Parameters: pointer to variable containing the port to connect on
- * Returns: the socket */
-/**********************************************************************/
-int startup(u_short *port) {
-    int httpd = 0;
-    struct sockaddr_in name;
+/************************************************/
+/*                                              */
+/* 返回监听套接字，参数为指向端口号的指针       */
+/*                                              */
+/************************************************/
+int get_server_socket(u_short *server_port) {
+    int server_httpd;
+    struct sockaddr_in server_name;
 
-    httpd = socket(PF_INET, SOCK_STREAM, 0);
-    if (httpd == -1)
+    server_httpd = socket(PF_INET, SOCK_STREAM, 0);
+    if (server_httpd == -1)
         error_die("socket");
-    memset(&name, 0, sizeof(name));
-    name.sin_family = AF_INET;
-    name.sin_port = htons(*port);
-    name.sin_addr.s_addr = htonl(INADDR_ANY);
-    if (bind(httpd, (struct sockaddr *)&name, sizeof(name)) < 0)
+    memset(&server_name, 0, sizeof(server_name));
+    server_name.sin_family = AF_INET;
+    server_name.sin_port = htons(*server_port);
+    server_name.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(server_httpd, (struct sockaddr *)&server_name, sizeof(server_name)) < 0)
         error_die("bind");
-    if (*port == 0)  /* if dynamically allocating a port */ {
+    if (*server_port == 0) {
         /* int namelen = sizeof(name); */
-        socklen_t namelen = sizeof(name);
-        if (getsockname(httpd, (struct sockaddr *)&name, &namelen) == -1)
+        socklen_t server_name_len = sizeof(server_name);
+        if (getsockname(server_httpd, (struct sockaddr *)&server_name, &server_name_len) == -1)
             error_die("getsockname");
-        *port = ntohs(name.sin_port);
+        *server_port = ntohs(server_name.sin_port);
     }
-    if (listen(httpd, 5) < 0)
+    if (listen(server_httpd, 5) < 0)
         error_die("listen");
-    return(httpd);
+    return server_httpd;
 }
 
-/**********************************************************************/
-/* Inform the client that the requested web method has not been
- * implemented.
- * Parameter: the client socket */
-/**********************************************************************/
+/************************************/
+/*                                  */
+/*  客户端所请求的方法并未实现      */
+/*                                  */
+/************************************/
 void unimplemented(int client) {
     char buf[1024];
 
@@ -451,29 +397,28 @@ void unimplemented(int client) {
 /**********************************************************************/
 
 int main(void) {
-    int server_sock = -1;
-    u_short port = 0;
-    int client_sock = -1;
-    struct sockaddr_in client_name;
-    /* int client_name_len = sizeof(client_name);*/
-    socklen_t client_name_len = sizeof(client_name);
-    pthread_t newthread;
+    int         server_sock;
+    u_short     server_port = 0;
+    int         *client_sock;
+    pthread_t   newthread;
 
-    server_sock = startup(&port);
-    printf("httpd running on port %d\n", port);
+    server_sock = get_server_socket(&server_port);
+    printf("httpd running on port %d\n", server_port);
 
     while (1) {
-        client_sock = accept(server_sock, (struct sockaddr *)&client_name, &client_name_len);
-        if (client_sock == -1)
+        /* 为避免多线程竞争问题，使用动态分配内存，在线程函数中释放  */
+        client_sock = (int*)malloc(sizeof(int));
+        if (client_sock == NULL)
+            error_die("malloc");
+        *client_sock = accept(server_sock, NULL, NULL);
+        if (*client_sock == -1)
             error_die("accept");
- /* accept_request(client_sock); */
-        /*if (pthread_create(&newthread , NULL, accept_request, client_sock) != 0)*/
-        if (pthread_create(&newthread , NULL, accept_request, (void*)(&client_sock)) != 0)
+        if (pthread_create(&newthread, NULL, accept_request, client_sock) != 0)
             perror("pthread_create");
     }
 
     close(server_sock);
 
-    return(0);
+    return 0;
 }
 
